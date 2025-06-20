32307ba3b85fd3acb2af514845982a92ccc1e3ca
Victor Rivera <victorray82@gmail.com>
2025-06-20 13:09:59 -0500

    Navbar update test

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,43 @@
 // src/components/Navbar.js
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, Link } from "react-router-dom";
-import styles from "./Navbar.module.css"; // Make sure to use a CSS module!
+import styles from "./Navbar.module.css"; // Use your CSS module!
 
 export default function Navbar({ showTabs, mode, onModeChange }) {
   const location = useLocation();
   const isMarkets = location.pathname === "/markets";
+  const [mobileOpen, setMobileOpen] = useState(false); // for hamburger, future use
 
   return (
     <nav className={styles.navbar}>
+      {/* Left: Logo + Brand, always */}
+      <div className={styles.navLeft}>
+        <Link to="/" className={styles.brandBtn} style={{ display: "flex", alignItems: "center", textDecoration: "none" }}>
+          <img
+            src="/logo192.png"
+            alt="OddsSightSeer Logo"
+            className={styles.logo}
+            style={{ width: 32, height: 32, marginRight: 10, verticalAlign: "middle" }}
+          />
+          <span className={styles.brand}>OddsSightSeer</span>
+        </Link>
+      </div>
+
+      {/* Hamburger for mobile */}
+      <button
+        className={styles.hamburger}
+        aria-label="Open menu"
+        onClick={() => setMobileOpen((v) => !v)}
+        style={{ marginLeft: "auto", display: "none" }}
+      >
+        <span />
+        <span />
+        <span />
+      </button>
+
+      {/* Center: Only on Markets page */}
       {isMarkets ? (
         <div className={styles.centerWrap}>
-          <Link to="/" className={styles.brandBtn}>
-            OddsSightSeer
-          </Link>
           <div className={styles.viewToggle}>
             <button
               className={`${styles.vtab} ${mode === "game" ? styles.active : ""}`}
@@ -30,17 +54,11 @@ export default function Navbar({ showTabs, mode, onModeChange }) {
           </div>
         </div>
       ) : (
-        <>
-          <div className={styles.navLeft}>
-            <Link to="/" className={styles.brandBtn}>
-              OddsSightSeer
-            </Link>
-          </div>
-          <div className={styles.navLinks}>
-            <Link to="/" className={styles.link}>Home</Link>
-            <Link to="/markets" className={styles.link}>Markets</Link>
-          </div>
-        </>
+        // Right links: Home/Markets (not on Markets page)
+        <div className={styles.navLinks}>
+          <Link to="/" className={styles.link}>Home</Link>
+          <Link to="/markets" className={styles.link}>Markets</Link>
+        </div>
       )}
     </nav>
   );
