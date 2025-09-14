b0ee7ab2d9ca04facc49c33d0f764b1afe949cd7
Victorrray <victorray82@gmail.com>
2025-09-13 22:13:26 -0700

    2.60 - Updated version number

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -621,7 +621,7 @@ export default function Account() {
 
       {/* Version info */}
       <div className="version-info">
-        <span>Version 2.59</span>
+        <span>Version 2.60</span>
       </div>
 
       <MobileBottomBar active="profile" showFilter={false} />
