3a7fac5840022787bf0dcab6ee2225969e0a7ed8
Victorrray <victorray82@gmail.com>
2025-10-02 14:54:02 -0700

    Fix: Desktop add button now opens bet slip instead of legacy My Picks

--- client/src/components/betting/OddsTable.js
+++ client/src/components/betting/OddsTable.js
@@ -2576,9 +2576,13 @@ export default function OddsTable({
                         className="desktop-add-pick-btn"
                         onClick={(e) => {
                           e.stopPropagation();
-                          addToPicks(row, { bookmaker: row.bk, book: row.bk?.title }, false, e.target);
+                          if (onAddBet) {
+                            addToBetSlip(row, { bookmaker: row.bk, book: row.bk?.title }, e.target);
+                          } else {
+                            addToPicks(row, { bookmaker: row.bk, book: row.bk?.title }, false, e.target);
+                          }
                         }}
-                        title="Add to My Picks"
+                        title={onAddBet ? "Add to Bet Slip" : "Add to My Picks"}
                       >
                         +
                       </button>
