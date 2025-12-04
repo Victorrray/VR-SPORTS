cc140868468c9929bc23e74dabcb3ce735634a82
Victorrray <victorray82@gmail.com>
2025-12-04 10:31:32 -0800

    Allow player prop pick text to wrap to multiple lines instead of truncating

--- client/src/components/design12/PlayerPropsPage.tsx
+++ client/src/components/design12/PlayerPropsPage.tsx
@@ -1218,7 +1218,7 @@ export function PlayerPropsPage({ onAddPick, savedPicks = [] }: { onAddPick: (pi
 
                     {/* Team/Line - Recommended pick (spread, total, moneyline, etc.) */}
                     <div className="lg:col-span-3 min-w-0">
-                      <div className={`${isLight ? 'text-gray-900' : 'text-white'} font-bold text-sm lg:text-base truncate`}>{pick.pick}</div>
+                      <div className={`${isLight ? 'text-gray-900' : 'text-white'} font-bold text-sm lg:text-base leading-tight`}>{pick.pick}</div>
                     </div>
 
                     {/* Book - Best sportsbook for this pick */}
