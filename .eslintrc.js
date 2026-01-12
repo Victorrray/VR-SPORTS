b3073554fd8defd7a87c9a25baca284e79f38579
Victorrray <victorray82@gmail.com>
2026-01-12 13:37:47 -0800

    Fix CI: add ESLint and Prettier configs and dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'client/',
+    'Figma Design.12/',
+    'coverage/',
+    'logs/',
+  ],
+};
