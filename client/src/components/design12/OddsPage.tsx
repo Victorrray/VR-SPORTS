18a11e52b542f6cea648352b41c317ec2d448751
Victorrray <victorray82@gmail.com>
2026-02-26 13:06:53 -0800

    Fix filter panel positioning - align with sidebar

--- client/src/components/design12/OddsPage.tsx
+++ client/src/components/design12/OddsPage.tsx
@@ -1462,7 +1462,7 @@ export function OddsPage({ onAddPick, savedPicks = [], betType, onBetTypeChange 
           {/* Backdrop - Desktop only (to the right of panel) */}
           <div 
             className={`hidden lg:block fixed right-0 bottom-0 bg-black/40 z-[9998] transition-all ${isFilterClosing ? 'opacity-0 duration-150' : 'opacity-100 duration-500 delay-100'}`}
-            style={{ left: '320px', top: '0' }}
+            style={{ left: 'calc(224px + 288px)', top: '0' }}
             onClick={closeFilterMenu}
           />
           
@@ -1475,10 +1475,11 @@ export function OddsPage({ onAddPick, savedPicks = [], betType, onBetTypeChange 
           
           {/* Side Panel - Desktop / Bottom Drawer - Mobile */}
           <div 
-            className={`!fixed max-lg:!bottom-0 max-lg:!left-0 max-lg:!right-0 max-lg:!top-auto lg:!left-0 lg:!bottom-0 max-lg:max-h-[85vh] ${isLight ? 'bg-white' : 'bg-slate-950'} lg:border-r max-lg:border-t ${isLight ? 'border-gray-200' : 'border-white/10'} lg:rounded-none max-lg:rounded-t-2xl flex flex-col ${isFilterClosing ? 'animate-out max-lg:slide-out-to-bottom lg:slide-out-to-left duration-200 ease-out fill-mode-forwards' : 'animate-in max-lg:slide-in-from-bottom lg:slide-in-from-left duration-300 ease-out'} lg:w-72 max-lg:w-full shadow-xl`}
+            className={`!fixed max-lg:!bottom-0 max-lg:!left-0 max-lg:!right-0 max-lg:!top-auto lg:!bottom-0 max-lg:max-h-[85vh] ${isLight ? 'bg-white' : 'bg-slate-950'} lg:border-r max-lg:border-t ${isLight ? 'border-gray-200' : 'border-white/10'} lg:rounded-none max-lg:rounded-t-2xl flex flex-col ${isFilterClosing ? 'animate-out max-lg:slide-out-to-bottom lg:slide-out-to-left duration-200 ease-out fill-mode-forwards' : 'animate-in max-lg:slide-in-from-bottom lg:slide-in-from-left duration-300 ease-out'} lg:w-72 max-lg:w-full shadow-xl`}
             style={{
               zIndex: 9999,
               top: '0',
+              left: '224px',
             }}
           >
             {/* Sticky Header */}
